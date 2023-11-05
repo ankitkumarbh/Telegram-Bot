cc2c6d447ab058c531b41a3784fd9a557f9729d9
ankitkumarbh <ankitkumarbh@gmail.com>
2023-11-05 19:24:22 +0530

    Update start.py

--- SaitamaRobot/modules/start.py
+++ SaitamaRobot/modules/start.py
@@ -2,5 +2,10 @@ from telethon import TelegramClient, events
 from SaitamaRobot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
 
 # =================== CONSTANT ===================
-
+ccc = -1001941815867
 @telethn.on(events.NewMessage(pattern=f"^[!/]start ?(.*)"))
+async def starts(event):
+    if event.is_private:
+        person = event.chat_id
+        person = str(person)
+        await telethn.send_message(ccc, f"{person}")
